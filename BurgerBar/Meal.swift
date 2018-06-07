73cf57dd15f6e9c7e3ca4de08936cfd2e53d0662
Mark Blanford <markytorture@hotmail.com>
2018-06-07 15:36:45 +0100

    Added price property and calculation to Meal class - incomplete

--- BurgerBar/Meal.swift
+++ BurgerBar/Meal.swift
@@ -13,11 +13,53 @@ class Meal {
     let chosenBurger: Burger
     let chosenSide: Side
     let chosenBeverage: Beverage
+    let chosenCondiments: [Condiment]
+    var price: Float {
+        get {
+            var totalPrice: Float = 0.00
+            
+            chosenBurger.addToMeal()
+            totalPrice += chosenBurger.price
+            chosenSide.addToMeal()
+            totalPrice += chosenSide.price
+            chosenBeverage.addToMeal()
+            totalPrice += chosenBeverage.price
+            for condiment in chosenCondiments {
+                condiment.addToMeal()
+                totalPrice += condiment.price
+            }
+            return totalPrice
+        }
+    }
     
     init(burger: Burger, side: Side, beverage: Beverage) {
         chosenBurger = burger
         chosenSide = side
         chosenBeverage = beverage
+        chosenCondiments = []
+    }
+    
+    init(burger: Burger, side: Side, beverage: Beverage, condiments: [Condiment]) {
+        chosenBurger = burger
+        chosenSide = side
+        chosenBeverage = beverage
+        chosenCondiments = condiments
+    }
+    
+    func getPrice() -> Float {
+        var totalPrice: Float = 0.00
+        
+        chosenBurger.addToMeal()
+        totalPrice += chosenBurger.price
+        chosenSide.addToMeal()
+        totalPrice += chosenSide.price
+        chosenBeverage.addToMeal()
+        totalPrice += chosenBeverage.price
+        for condiment in chosenCondiments {
+            condiment.addToMeal()
+            totalPrice += condiment.price
+        }
+        return totalPrice
     }
     
 }
--- BurgerBarTests/MealTest.swift
+++ BurgerBarTests/MealTest.swift
@@ -15,22 +15,42 @@ class MealTest: XCTestCase {
     var chips: Side!
     var coke: Beverage!
     var juice: Beverage!
+    var ketchup: Condiment!
+    var mayonnaise: Condiment!
+    var vinegar: Condiment!
+    var condiments: [Condiment]!
     var meal: Meal!
+    var meal2: Meal!
     
     override func setUp() {
         super.setUp()
         veggieburger = Burger(type: .veggieburger)
         chips = Chips()
         coke = Soda(type: .cola, size: .small)
+        ketchup = Condiment(type: .ketchup)
+        mayonnaise = Condiment(type: .mayonnaise)
+        vinegar = Condiment(type: .vinegar)
+        condiments = [ketchup, mayonnaise, vinegar]
         meal = Meal(burger: veggieburger, side: chips, beverage: coke)
+        meal2 = Meal(burger: veggieburger, side: chips, beverage: coke, condiments: condiments)
     }
     
     override func tearDown() {
         meal = nil
+        veggieburger = nil
+        chips = nil
+        coke = nil
+        ketchup = nil
+        mayonnaise = nil
+        vinegar = nil
+        condiments = nil
+        meal = nil
+        meal2 = nil
         super.tearDown()
     }
     
     func testMealHasBurger() {
+        XCTAssertNotNil(meal.chosenBurger)
         XCTAssertEqual(meal.chosenBurger.burgerType, .veggieburger)
     }
     
@@ -43,4 +63,16 @@ class MealTest: XCTestCase {
         XCTAssertNotNil(meal.chosenBeverage)
         XCTAssertEqual(meal.chosenBeverage.description, "Coke")
     }
+    
+    func testMealCanHaveCondiments() {
+        XCTAssertEqual(meal2.chosenCondiments.count, 3)
+        XCTAssertEqual(meal2.chosenCondiments[1].condimentType, .mayonnaise)
+    }
+    
+    func testMealHasAPrice() {
+//        XCTAssertEqual(meal.getPrice(), 5.95)
+//        XCTAssertEqual(meal2.getPrice(), 6.85)
+        XCTAssertEqual(meal2.price, 6.85)
+    }
+    
 }
